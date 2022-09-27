3cfc4e168760f70768864d786cba1084bff942f1
Morijin2468 <madehinenoch@gmail.com>
2022-09-27 03:00:26 +0000

    data structures tasks done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-    for elem in my_list:
-        print("{:d}".format(elem))
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
